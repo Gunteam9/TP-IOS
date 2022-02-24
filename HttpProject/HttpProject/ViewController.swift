dd7dc94bd6dc7bba2516db65b2f8ae1a8d2ba9e9
tplocal <tplocal@MacBook-Air-9.local>
2022-02-24 08:47:44 +0100

    Add comment HTTPProject

--- HttpProject/HttpProject/ViewController.swift
+++ HttpProject/HttpProject/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         
         Task.init {
             await getTextFromURL(url: "https://eddbali.net/files/texte.txt");
+            // Un autre URL que la météo pour éviter de créer un compte sur un site random
             await getJsonFromURL(url: "https://jsonplaceholder.typicode.com/todos/1");
         }
         
@@ -28,12 +29,13 @@ class ViewController: UIViewController {
     }
     
     func getJsonFromURL(url: String) async {
+        // On execute la requête puis on print les données en brut
         let data: Data? = await executeRequest(urlString: url);
-        print("here1");
         print(String(data: data!, encoding: .utf8)!);
         if data != nil {
             let jsonDecoder = JSONDecoder();
             do {
+                // On décode le Json pour le lire facilement
                 let todoOne = try jsonDecoder.decode(JsonPlaceHolderTodoOne.self, from: data!);
                 print(todoOne.description);
             } catch {
@@ -44,39 +46,17 @@ class ViewController: UIViewController {
     
     func executeRequest(urlString: String) async -> Data? {
         // Créer l'URL d'où viennent les données
-        let url = URL(string: urlString)
-        // Créer une session de connexion
-//        let session = URLSession(configuration: .default)
-//        var res: Data?;
-//        // Créer une tâche
-//        let tache = session.dataTask(with: url!)
-//        { (data, response, error) in
-//            if error != nil {
-//                print("Error: ", error!);
-//                return;
-//            } else {
-//                if let data = data {
-//                    print("here2");
-//                    print(String(data: data, encoding: .utf8)!);
-//                    res = data;
-//                } else {
-//                    print("Pas de données");
-//                }
-//            }
-//        }
-//        // Lancer la tâche
-//        tache.resume()
-//
-//        return res;
-
-        
+        let url = URL(string: urlString);
         do {
             let (data, response) = try await URLSession.shared.data(for: URLRequest(url: url!));
             let statusCode: Int = (response as? HTTPURLResponse)!.statusCode;
+            // On check si la request est un succès
             guard statusCode >= 200 && statusCode < 300 else
                 {
-                throw RuntimeException.runtimeError("HTTP Error " + String(statusCode));
+                    // Si ce n'est pas le cas, on stop le programme
+                    throw RuntimeException.runtimeError("HTTP Error " + String(statusCode));
                 }
+            // Si c'est le cas, on return les données récupérées
             return data;
         } catch {
             print("Error: ", error);
@@ -86,6 +66,7 @@ class ViewController: UIViewController {
     }
 }
 
+// Les Runtime Exception n'existe pas vraiment en swift
 enum RuntimeException: Error {
     case runtimeError(String)
 }
@@ -96,6 +77,7 @@ class JsonPlaceHolderTodoOne: Decodable, CustomStringConvertible {
     let title: String;
     let completed: Bool;
     
+    // Equivalent du toString de Java
     public var description: String {
         return "JsonPlaceHolderTodoOne{userId: \(userId), id:\(id), title: \(title), completed: \(completed)}"}
 }
